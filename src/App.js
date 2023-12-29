d3c334716ae7d5abf5b0c603136f2835e1601f87
Nikolau02 <nicklourenc@gmail.com>
2023-12-29 16:34:22 -0300

    Projeto das Aulas Práticas de ReactJS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom' //sofreu mudanças
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer';
+import Projects from './components/pages/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path='/'>
+            <Route exact path='/' element={<Home />} />
+          </Route>
+          <Route exact path='/projects'>
+            <Route exact path='/projects' element={<Projects />} />
+          </Route>
+          <Route exact path='/company'>
+            <Route exact path='/company' element={<Company />} />
+          </Route>
+          <Route exact path='/contact'>
+            <Route exact path='/contact' element={<Contact />} />
+          </Route>
+          <Route exact path='/newproject'>
+            <Route exact path='/newproject' element={<NewProject />} />
+          </Route>
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
   );
 }
 
